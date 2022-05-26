67869e6bd85b4cfffd26467fa4e5a17b5e7601e7
Lucy Korte <lucy.n.korte.25@dartmouth.edu>
2022-05-26 01:42:57 -0400

    commiting changes to board

--- board.h
+++ board.h
@@ -1,14 +1,8 @@
 /* 
  * board.h - header file for Sudoku board module
- *
- * A "board" is a
- *  set of counters, each distinguished by an 
- * integer key. It's a set - each key can only occur once in the set 
- * - but instead of storing (key,item) pairs, it tracks a counter 
- * for each key.  It starts empty. Each time `counters_add` is 
- * called on a given key, that key's counter is incremented. The 
- * current counter value can be retrieved by asking for the relevant 
- * key.
+ * 
+ * A "board" is a struct holding an array of arrays of slot structs
+ * 
  * 
  * CS50
  */
