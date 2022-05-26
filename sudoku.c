c0e930abb28966adde8d9d64b49a5d9a212ae3e8
Ryan S. Kim <ryan.s.kim.25@dartmouth.edu>
2022-05-26 13:02:46 -0400

    started with the logic of the solve recursive algorithm; code is more for the logic and to find an algorithm.

--- sudoku.c
+++ sudoku.c
@@ -10,13 +10,126 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "board.h"
+#include "board.c"
 
-void sudoku_solve() {
-  
+void sudoku_solve(board_t* board) {
+    /*
+    1. Build sudoku puzzle from stdin
+    2. Recurse through every slot
+    */
+    
 }
 
-*sudoku_t build_sudoku() {
+bool solve_recurse() {
+    /*
+    1. for each slot; check if it changeable or not
+        2. if changeable:
+            solve_recurse(row, col, slot):
+            int i = 1
+            while (recurse_helper(i) returns false)
+                if (i == 9), quit (error)
+                i++
+
+    */
+    slot_t* *slist[]; // holds a list of all the non-given slots in order
+
+    solve_recurse(board, row, col);
+
+    if (row == 9) {
+
+    }
+
+    for (int i = 1; i < 10; i++) {
+        if (valid_input(board, i, row, num)) {
+            if (solve_recurse(board, row, col)) {
+                board[row][col]->num = i;
+                return true;
+            }
+        }
+        
+    }
+    
+
+    if (board[row][col]->given) {
+        // go to next slot
+        if (row < 8) {
+            
+        }
+    }
+
+    while (!valid_input(board, ans, row, col)) {
+        if (ans == 9) {
+            return false;
+        }
+        ans++;
+    }
+    if (solve_recurse(board, ans, row, col)) {
+        board[row][col]->num = ans;
+        return true;
+    }
+    else {
+        solve_recurse(board, ans+1, row, col);
+    }
+
+    if (ans < 9 && solve_recurse(board, ans++, row, col)) {
+
+    }
+
+
+    else if (ans < 9) {
+        solve_recurse(board, ans++, )
+    }
+
+    if (valid_input(board, ans, row, col) && row == 8 && col == 8) {
+        return true;
+    }
+
+
+    
+    while (!valid_input(board, ans, row, col)) {
+        if (ans == 9) {
+            return false;
+        }
+        ans++
+    }
+
+    if (row < 8) {
+        solve_recurse(board, 1, row++, col);
+    }
+    else if (col < 8) {
+        solve_recurse(board, 1, 0, col++);
+    }
+    else {
+
+    }
+    solve_recurse(board, 1, row++)
+
+
+
+    if (valid_input(board, ans, row, col)) {
+        if (row == 8 && col == 8) {
+            return true;
+        }
+        if (row < 9) {
+            solve_recurse(board, 1, row++, col);
+        }
+        else if (col < 9) {
+            solve_recurse(board, 1, 0, col++)
+        }
+    }
+    else {
+        solve_recurse(board, ans++, row, col);
+    }
+}
+
+void recurse_helper() {
+    /*
+    int i = 1
+
+    */
+}
+
+sudoku_t* build_sudoku() {
     sudoku_t* sudoku = sudoku_new();
     int num;
     while (scanf("%d", &num) == 1) {
