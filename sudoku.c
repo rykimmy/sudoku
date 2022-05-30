42d2f540e429f2355f08fa22cfa94e3c9dd33c6a
Lucy Korte <lucy.n.korte.25@dartmouth.edu>
2022-05-29 23:04:46 -0400

    sudoku checks for multiple solutions

--- sudoku.c
+++ sudoku.c
@@ -62,17 +62,61 @@ static bool solver(board_t* sudoku)
     return false;
 }
 
-bool sudoku_solve(board_t* sudoku) {
-    
-    if (solver(sudoku)) {
-        board_print(sudoku);
+static bool solver_backward(board_t* sudoku)
+{
+    int row, col;
+
+    //goes through sudoku, checks if there are empty spots
+    //if empty, parameter keeps the row/column through param
+    if (!empty_location(sudoku, &row, &col)) {
+        
         return true;
     }
-    else {
-        fprintf(stderr, "sudoku not solvable\n");
-        return false;
+
+
+    //using the row/column values from empty_location()
+    for (int num = 9; num > 0; num--) {
+        // printf("trying %d in row %d col %d\n", num, row, col);
+        if (valid_input(sudoku, row, col, num)) {
+
+            //tentatively set
+            // sudoku->grid[row][col]->num = num;
+            // printf("putting %d at row %d col %d\n", num, row, col);
+            board_set(sudoku, row, col, num, false);
+            // printf("put %d in row %d col %d\n", num, row, col);
+            
+            // board_print(sudoku);
+            // printf("------------------\n");
+
+            //continue with solving, if works, return true, success
+            if (solver(sudoku)) {
+                return true;
+            }
+            //doesn't work, go back to 0
+            board_set(sudoku, row, col, 0, false);
+        
+        }
     }
+    return false;
 }
+
+
+bool unique_solution(board_t *sudoku) {
+    board_t *copy_forward = board_copy(sudoku);
+    board_t *copy_backward = board_copy(sudoku);
+    solver(copy_forward);
+    solver_backward(copy_backward);
+    for (int i=0; i < 9; i++){
+        for (int j=0; j < 9; j++){
+            if (board_get(copy_forward, i, j) != board_get(copy_backward, i, j)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 board_t *build_sudoku() 
 {
     board_t* sudoku = board_new();
@@ -88,7 +132,37 @@ board_t *build_sudoku()
     return sudoku;
 }
 
+bool sudoku_solve(board_t* sudoku) {
+    
+    if (solver(sudoku)) {
+        board_print(sudoku);
+        return true;
+    }
+    else {
+        fprintf(stderr, "sudoku not solvable\n");
+        return false;
+    }
+}
+
+bool sudoku_solve_backward(board_t* sudoku) {
+    
+    if (solver_backward(sudoku)) {
+        board_print(sudoku);
+        return true;
+    }
+    else {
+        fprintf(stderr, "sudoku not solvable\n");
+        return false;
+    }
+}
+
 int main () {
     board_t *sudoku = build_sudoku();
-    sudoku_solve(sudoku);
+   
+    if (unique_solution(sudoku)) {
+        printf("unique solution\n");
+    }
+    else {
+        printf("multiple solutions\n");
+    }
 }
